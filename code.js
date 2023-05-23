a388df94c72a5d873682c8b18953eedd7a16132c
Dario Cruz <rio2012@gmail.com>
2023-05-23 11:25:13 -0400

    Add classes for treeNodes and BST. Working on methods needed to build the tree.

---
+++ code.js
@@ -0,0 +1,22 @@
+// Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
+class treeNode {
+    constructor(inputData, leftChild = null, rightChild = null) {
+        this.inputData = inputData
+        this.leftChild
+        this.rightChild
+    }
+}
+
+// Build a Tree class / factory which accepts an array when initialized. 
+// The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
+class tree {
+    constructor(givenArray) {
+        this.givenArray = givenArray
+        this.treeRoot = null
+    }
+    // Write a function which takes an array of data and turns it into a balanced binary tree,
+    // full of Node objects.
+    buildTree() {
+        this.givenArray
+    }
+}
